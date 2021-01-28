4fb4edc8c12b5bd62a66e0a86373b6b1eff6286d
Oksana-dnp <71383222+Oksana-dnp@users.noreply.github.com>
2021-01-28 16:18:56 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
-var radiusFigure;
-
-radiusFigure = prompt('Введите радиус цилиндра');
+var radiusFigure = prompt('Введите радиус цилиндра');
 
 console.log(radiusFigure);
 
-var heightFigure;
-
-heightFigure = prompt('Введите высоту цилиндра');
+var heightFigure = prompt('Введите высоту цилиндра');
 
 console.log(heightFigure);
 
@@ -14,7 +10,5 @@ var areaFigure = Math.PI * Math.sqrt(radiusFigure);
 
 console.log(areaFigure);
 
-//можно ли так переносить длинный код?
-confirm('Объем цилиндра с площадью основы ' + areaFigure + ', '
-    + 'радиусом ' + radiusFigure + ' ' + 'и высотой ' + heightFigure + ' '
-    + 'равен ' + (Math.PI * radiusFigure * heightFigure));
+//можно ли так переносить длинный код на следующую строку?
+confirm('Объем цилиндра с площадью основы ' + areaFigure + ', ' + 'радиусом ' + radiusFigure + ' ' + 'и высотой ' + heightFigure + ' ' + 'равен ' + (Math.PI * radiusFigure * heightFigure));
